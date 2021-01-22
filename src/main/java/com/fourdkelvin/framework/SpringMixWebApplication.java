d17421cb89f7029013b3e810f0f52fdc2bfffb19
Kelvin <4dkelvin@gmail.com>
2021-01-22 11:43:26 +0800

    fast commit

---
+++ src/main/java/com/fourdkelvin/framework/SpringMixWebApplication.java
@@ -0,0 +1,13 @@
+package com.fourdkelvin.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMixWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMixWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourdkelvin/framework/SpringMixWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourdkelvin.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMixWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
