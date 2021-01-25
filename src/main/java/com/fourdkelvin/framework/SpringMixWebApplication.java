d3d2021946b8a8d5d1fafed26951cce3ead0c510
Kelvin <4dkelvin@gmail.com>
2021-01-25 17:06:42 +0800

    tomcat deploy

--- src/main/java/com/fourdkelvin/framework/SpringMixWebApplication.java
+++ src/main/java/com/fourdkelvin/framework/SpringMixWebApplication.java
@@ -1,13 +1,21 @@
 package com.fourdkelvin.framework;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMixWebApplication {
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringMixWebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringMixWebApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMixWebApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMixWebApplication.class, args);
+    }
 }
