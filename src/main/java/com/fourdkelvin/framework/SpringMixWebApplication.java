4aa4b4f0e72935d1bc3892b1b74842bcce4e5df6
Kelvin <4dkelvin@gmail.com>
2021-02-01 12:15:34 +0800

    fast commit

--- src/main/java/com/fourdkelvin/framework/SpringMixWebApplication.java
+++ src/main/java/com/fourdkelvin/framework/SpringMixWebApplication.java
@@ -8,13 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.fourdkelvin.framework")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class SpringMixWebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringMixWebApplication.class);
-    }
-
+public class SpringMixWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringMixWebApplication.class, args);
     }
